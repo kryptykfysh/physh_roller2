c8d101205817e3049f83b0bb4d42da06036e8774
Kryptykfysh <kryptykfysh@kryptykfysh.co.uk>
2014-11-03 09:19:54 +1100

    more Die testing.

--- lib/physh_roller/die.rb
+++ lib/physh_roller/die.rb
@@ -2,10 +2,14 @@
 
 module PhyshRoller
   class Die
-    attr_reader :sides
+    attr_reader :sides, :history
 
     def initialize(sides:)
       @sides = sides
+    end
+
+    def roll
+      1
     end
   end
 end
--- spec/lib/physh_roller/die_spec.rb
+++ spec/lib/physh_roller/die_spec.rb
@@ -5,6 +5,17 @@ module PhyshRoller
     let(:die) { FactoryGirl.build :die }
     subject { die }
 
-    it { should respond_to :sides }
+    it { expect(die).to respond_to :sides }
+    it { expect(die).to respond_to :roll }
+    it { expect(die).to respond_to :history }
+
+    describe 'methods' do
+      describe '#roll' do
+        it { expect(die.roll).to be >= 1 }
+        it 'should be less than or equal to @sides' do
+          expect(die.roll).to be <= die.sides
+        end
+      end
+    end
   end
 end
